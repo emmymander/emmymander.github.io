d08aed0e929731a8c323dac40e51191c645b136f
Emmy <emmy.mander@hotmail.com>
2019-05-12 14:48:30 +0200

    commit 1

---
+++ assets/includes/navigation.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Emmy Mander Design</title>
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Font-Awesome Core CSS -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <!-- General Styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+    <!--================================================
+    Navigation
+    =================================================--> 
+    <header>
+        <!-- Include navigation-->
+        <?php include 'assets/includes/navigation.php';?>
+        <?php require_once 'assets/includes/navigation.php';?>
+        <nav class="navbar navbar-expand-md navbar-light">
+            <div class="container">
+                <button class="navbar-toggler" type="submit" data-toggle="collapse" data-target="#navbar-toggle"></button>
+                <a href="#" class="navbar-brand">
+                    <h1 class="navbar-title">Emmy Mander</h1>
+                </a>
+                <div class="collapse navbar-collapse" id="navbar-toggle">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a href="#" class="nav-link">Projects</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
